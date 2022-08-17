c2bb9d83f7a6cbc4ad5dc4700e6e1b2ac000b7dd
mrf-dot <mfeigenbaum23@gmail.com>
2022-08-17 14:50:39 -0400

    Made the site into a professional form.

---
+++ script/nav.js
@@ -0,0 +1,12 @@
+const pages = {
+	Home: "index",
+	Resume: "resume",
+	Projects: "repos"
+};
+document.writeln('<div class="nav"> <p><strong>Navigation</strong>');
+Object.keys(pages).forEach(function(page) {
+	for (var i = 0; i < 3; i++)
+		document.write('&nbsp;');
+	document.writeln('<a href="' + pages[page] + '.html">' + page + '</a> |');
+})
+document.writeln('</p></div>');
