04e0ed14c6ca3117fc2609b773f6e471142391fd
mrf-dot <mfeigenbaum23@gmail.com>
2022-08-17 15:22:31 -0400

    Made absolute references of html

--- script/nav.js
+++ script/nav.js
@@ -7,6 +7,6 @@ document.writeln('<div class="nav"> <p><strong>Navigation</strong>');
 Object.keys(pages).forEach(function(page) {
 	for (var i = 0; i < 3; i++)
 		document.write('&nbsp;');
-	document.writeln('<a href="' + pages[page] + '.html">' + page + '</a> |');
+	document.writeln('<a href="/' + pages[page] + '.html">' + page + '</a> |');
 })
 document.writeln('</p></div>');
