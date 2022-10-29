c54b6eaf8f4a98cb1852f961c93ab4daa7fe7f1d
mrf-dot <mfeigenbaum23@gmail.com>
2022-10-28 20:06:31 -0400

    Made a header

--- script/nav.js
+++ script/nav.js
@@ -1,14 +1,11 @@
-const SPACES = 5;
 const PAGES = {
 	Home: "index",
 	Resume: "resume",
 	Projects: "repos",
-	"Base Converter": "base-convert"
+	WebToys: "toys"
 };
-document.writeln('<div class="nav"> <p><strong>Navigation</strong>');
+document.writeln('<div class="nav">');
 Object.keys(PAGES).forEach(function(page) {
-	for (var i = 0; i < SPACES; i++)
-		document.write('&nbsp;');
-	document.writeln('<a href="/' + PAGES[page] + '.html">' + page + '</a> |');
+	document.writeln('<a href="' + PAGES[page] + '.html">' + page + '</a> ');
 })
-document.writeln('</p></div>');
+document.writeln('</div>');
