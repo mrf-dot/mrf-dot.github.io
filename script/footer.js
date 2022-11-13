6a59d97568058a6a3f81d18ff576ccafe32ea57a
mrf-dot <mfeigenbaum23@gmail.com>
2022-11-12 21:40:31 -0500

    Made header and footer combined

--- script/footer.js
+++
@@ -1,1 +0,0 @@
-document.write('</div><footer>&copy; ' + new Date().getFullYear() + ' <a href="mailto:mfeigenbaum23@gmail.com">Mitch Feigenbaum</a></footer>');
--- script/header.js
+++ script/header.js
@@ -4,8 +4,30 @@ const PAGES = {
 	Projects: "repos",
 	WebToys: "toys"
 };
-document.write('<div class="nav">');
-Object.keys(PAGES).forEach(function(page) {
-	document.write('<a href="/' + PAGES[page] + '.html">' + page + '</a> ');
-})
-document.write('</div><div class="container">');
+let nav, foot;
+
+function createNav() {
+	nav = document.createElement("div");
+	nav.id = "nav";
+	Object.keys(PAGES).forEach(function(page) {
+		var a = document.createElement("a");
+		a.href = "/" + PAGES[page] + ".html";
+		a.innerHTML = page;
+		nav.appendChild(a);
+	})
+}
+
+function createFoot() {
+	foot = document.createElement("footer");
+	foot.innerHTML = "&copy; " + new Date().getFullYear() + " ";
+	var a = document.createElement("a");
+	a.href = "mailto:mfeigenbaum23@gmail.com";
+	a.innerHTML="Mitch Feigenbaum";
+	foot.append(a);
+}
+
+createNav();
+createFoot();
+let container = document.getElementById("container")
+document.body.insertBefore(nav, container);
+document.body.appendChild(foot);
